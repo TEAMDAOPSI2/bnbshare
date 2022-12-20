a478fe9a09f6593b0cb65c02c1c23b4624dc2e36
Aditya Rizki Pratama <arizki717@gmail.com>
2022-12-20 20:31:18 +0700

    init day 5 [add new page sell with shares]

--- sections/sell-shares/NeighbourhoodSection.jsx
+++ sections/sell-shares/NeighbourhoodSection.jsx
@@ -1,22 +1,14 @@
-import BaliMarinaImg from 'public/assets/sell-with-stake/where/Dubai-Marina.png';
-import PalmJumeirahImg from 'public/assets/sell-with-stake/where/Palm-Jumeirah.png';
-import DowntownBaliImg from 'public/assets/sell-with-stake/where/Downtown-Dubai.png';
-import JumeirahBeachResidenceImg from 'public/assets/sell-with-stake/where/Jumeirah-Beach-Residence.png';
-import DIFCImg from 'public/assets/sell-with-stake/where/DIFC.png';
-import BusinessBayImg from 'public/assets/sell-with-stake/where/Business-Bay.png';
-import JumeirahandLaMerImg from 'public/assets/sell-with-stake/where/Jumeirah-and-LaMer.png';
-import BluewatersIslandImg from 'public/assets/sell-with-stake/where/Bluewaters-Island.png';
-import CityWalkImg from 'public/assets/sell-with-stake/where/City-Walk.png';
-import BaliCreekImg from 'public/assets/sell-with-stake/where/Dubai-Creek.png';
-import JumeirahLakeTowersImg from 'public/assets/sell-with-stake/where/Jumeriah-Lake-Towers.png';
-import TheGreensAndViewsImg from 'public/assets/sell-with-stake/where/The-Greens-The-Views.png';
+import PererranImg from 'public/assets/sell-with-stake/where/Pererran.jpg';
+import CangguImg from 'public/assets/sell-with-stake/where/canggu.jpg';
+import UbudImg from 'public/assets/sell-with-stake/where/ubud.avif';
+import UluwatuImg from 'public/assets/sell-with-stake/where/uluwatu.avif';
 
 const Box = (props) => {
   // eslint-disable-next-line react/prop-types
   const { title, image } = props;
   return (
     <div className="flex flex-col">
-      <img src={image} className="h-[192px] rounded" alt="" />
+      <img src={image} className="h-[230px] object-cover rounded" loading="lazy" alt="" />
       <h3 className="text-xl font-bold mt-3">{title}</h3>
     </div>
   );
@@ -32,18 +24,10 @@ const NeighbourhoodSection = () => (
         </p>
       </div>
       <div className="grid gap-10 md:grid-cols-4">
-        <Box title="Bali Marina" image={BaliMarinaImg.src} />
-        <Box title="Palm Jumeirah" image={PalmJumeirahImg.src} />
-        <Box title="Downtown Bali" image={DowntownBaliImg.src} />
-        <Box title="Jumeirah Beach Residence (JBR)" image={JumeirahBeachResidenceImg.src} />
-        <Box title="DIFC" image={DIFCImg.src} />
-        <Box title="Business Bay" image={BusinessBayImg.src} />
-        <Box title="Jumeirah and La Mer" image={JumeirahandLaMerImg.src} />
-        <Box title="Bluewaters Island" image={BluewatersIslandImg.src} />
-        <Box title="City Walk" image={CityWalkImg.src} />
-        <Box title="Bali Creek" image={BaliCreekImg.src} />
-        <Box title="Jumeriah Lake Towers (JLT)" image={JumeirahLakeTowersImg.src} />
-        <Box title="The Greens & The Views" image={TheGreensAndViewsImg.src} />
+        <Box title="Canggu" image={CangguImg.src} />
+        <Box title="Pererran" image={PererranImg.src} />
+        <Box title="Uluwatu" image={UluwatuImg.src} />
+        <Box title="Ubud" image={UbudImg.src} />
       </div>
     </div>
   </section>
